819ddd1a73ac9998ece14333ef9d6af42acfe2dc
pavel.maximov <pavel.maximov@rocket-internet.de>
2015-12-09 19:17:10 +0100

    Added Bourbon

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,17 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var livereload = require('gulp-livereload');
 
 gulp.task('sass', function () {
     gulp.src('./src/css/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./public/css/'));
+        .pipe(gulp.dest('./public/css/'))
+        .pipe(livereload());
 });
 
 gulp.task('sass:watch', function () {
+    livereload.listen();
     gulp.watch('./src/css/**/*.scss', ['sass']);
+    gulp.watch('./public/**/*.html', livereload.reload);
 });
