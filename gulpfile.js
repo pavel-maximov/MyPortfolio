2665bf54d402d94c5e16e19395ea6937cb5469fe
pavel.maximov <pavel.maximov@rocket-internet.de>
2016-02-13 00:46:17 +0100

    - added fonts - added normilize.css - started implementing the responsive layout

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,18 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var livereload = require('gulp-livereload');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function () {
     gulp.src('./src/css/**/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write('./maps'))
         .pipe(gulp.dest('./public/css/'))
         .pipe(livereload());
 });
 
-gulp.task('sass:watch', function () {
+gulp.task('default', function () {
     livereload.listen();
     gulp.watch('./src/css/**/*.scss', ['sass']);
     gulp.watch('./public/**/*.html', livereload.reload);
