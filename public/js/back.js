732b5fd1e68092c2f57da68cdf690ca0c748d153
pavelmaximov <pavel@maximov.it>
2016-04-10 01:24:48 +0100

    Overall text styles fixes

--- public/js/back.js
+++ public/js/back.js
@@ -30,13 +30,6 @@ var startPointSkills;
 var currentScroll = $('#bk-modal').scrollTop();
 
 
-//function reserResizing () {
-//    $html.css('font-size', '');
-//    $body.css('background-size', '');
-//    $target.css('width', '');
-//    $target.css('height', '');
-//}
-
 //
 //var code = '<article id="intro"><header class="article-header"><div class="article-title-wrapper"><figure class="article-title-img-wrapper"><img class="article-title-img" src="/img/head.png"></figure><h1>Pavel Maximov</h1><h2>Front-end Developer</h2><h3>London, UK</h3></div><nav class="article-nav"></nav></header><div class="article-body"><video id="intro-video" height="100%" width="100%" autoplay loop><source src="/video/intro.mp4" type="video/mp4"></video><div id="intro-code"></div></div></article>';
 //var printedChar = 0;
@@ -89,7 +82,7 @@ function getRandomArbitrary(min, max) {
 }
 
 function scrollToElement(selector){
-    $htmlBody.animate({scrollTop: $(selector).offset().top},'fast');
+    $htmlBody.animate({scrollTop: $(selector).offset().top}, 1500);
 }
 
 function onScrollAbout (scrollTop, windowHeight) {
@@ -136,8 +129,7 @@ function onScrollMainContainer (scrollTop, windowHeight, scrollPoints) {
     } else if (scrollTop < scrollPoints.outro) {
         $main.css('background-color', 'hsla(0,10%,100%,0.85)');
     } else {
-        newAlphaValue = 1 - (scrollTop - scrollPoints.outro) / (windowHeight / 2);
-        newAlphaValue = newAlphaValue < 0.2 ? 0.2 : newAlphaValue;
+        newAlphaValue = 1 - (scrollTop - scrollPoints.outro) / windowHeight;
         newAlphaValue = newAlphaValue > .85 ? .85 : newAlphaValue;
         $main.css('background-color', 'hsla(0,10%,100%,' + newAlphaValue + ')');
     }
@@ -148,7 +140,7 @@ function onScroll(event) {
     var windowHeight = $window.height();
     var scrollPoints = {
         skills: windowHeight,
-        outro: $outro.offset().top - (windowHeight / 2)
+        outro: $outro.offset().top - windowHeight
     };
 
     onScrollAbout(scrollTop, windowHeight);
@@ -265,7 +257,7 @@ $document.ready(function () {
     startPointExperience = $experience.offset().top;
     endPointExperience = startPointExperience + $experience.outerHeight() - $window.height();
     startPointAbout = $about.offset().top;
-    endPointAbout = startPointAbout + $about.outerHeight() - $window.height();
+    endPointAbout = startPointAbout + $about.outerHeight() - ($window.height() / 2);
     startPointSkills = $skills.offset().top;
     endPointSkills = startPointSkills + $skills.outerHeight();
 
@@ -355,67 +347,3 @@ $document.ready(function () {
 
     initCarousel($('#references'), $('#references-main-carousel'));
 });
-
-//
-//!function (t) {
-//    t.MDChapter = function (e, n) {
-//        var o = (t.extend({}, t.fn.MDChapter.defaults, n), t(e)), i = t(".chapter__title", o), a = (t(".chapter__content", o), function () {
-//            r()
-//        }), r = function () {
-//            t(window).on("scroll", c), t(window).on("resize", c), c(null)
-//        }, c = function () {
-//            var e = o.outerHeight(), n = o.offset().top, a = t(window).height(), r = t(window).scrollTop();
-//            if (r >= n && n + e - a >= r)i.addClass("title--fixed").css({top: 0}); else {
-//                var c = r - n;
-//                c = Math.max(0, c), c = Math.min(e - a, c), i.removeClass("title--fixed").css({top: c})
-//            }
-//        };
-//        a()
-//    }, t.fn.MDChapter = function (e) {
-//        return this.each(function () {
-//            if (void 0 === t(this).data("MDChapter")) {
-//                var n = new t.MDChapter(this, e);
-//                t(this).data("MDChapter", n)
-//            }
-//        })
-//    }, t.fn.MDChapter.defaults = {option1: 1e3, option2: 760}
-//}(jQuery), function (t) {
-//    t.MDProject = function (e, n) {
-//        var o = (t.extend({}, t.fn.MDProject.defaults, n), t(e)), i = t(".project__header", o), a = t(".project__content", o), r = this, c = !1;
-//        this.closeProject = function () {
-//            r.collapse(), o.trigger("close", {$project: o}), c = !1
-//        }, this.openProject = function (t) {
-//            f(t)
-//        };
-//        var s = function () {
-//            h()
-//        }, h = function () {
-//            t(window).on("scroll", l)
-//        };
-//        this.collapse = function (e) {
-//            return "undefined" == typeof e && (e = !0), e ? (a.animate({height: 0}, 800), t(".header__image", i).animate({top: 0}, 800)) : (a.css({height: 0}), t(".header__image", i).css({top: 0})), i.css("height", t(window).height()), o.removeClass("open"), c = !1, 800
-//        }, this.expand = function () {
-//            return o.addClass("open"), a.css("height", "auto"), i.css("height", .54 * t(window).height()), window.viewportUnitsBuggyfill.refresh(), c = !0, 600
-//        };
-//        var f = function (e) {
-//            o.trigger("loading"), t.ajax({url: e, success: u})
-//        }, u = function (e) {
-//            var n = t(textToHtml(e)), o = t(".project__content", n);
-//            a.css("height", "0px").html(o.html()).waitForImages(d, function () {
-//            })
-//        }, d = function () {
-//            o.trigger("loaded", {$project: o})
-//        }, l = function () {
-//            var e = (t(window).scrollTop() - o.offset().top) / i.height();
-//            c && t(".header__image", i).css({top: .5 * Math.min(1, Math.max(0, e)) * i.height()})
-//        };
-//        s()
-//    }, t.fn.MDProject = function (e) {
-//        return this.each(function () {
-//            if (void 0 === t(this).data("MDProject")) {
-//                var n = new t.MDProject(this, e);
-//                t(this).data("MDProject", n)
-//            }
-//        })
-//    }, t.fn.MDProject.defaults = {option1: 1e3, option2: 760}
-//}(jQuery);
